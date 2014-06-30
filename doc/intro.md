1c89d6d62f993d0e925f0177deb9b7552398ef03
Anton Kalyaev <anton.kalyaev@gmail.com>
2014-06-30 19:14:22 +0400

    new lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kaize-tv
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/kaize_tv/core_test.clj
@@ -0,0 +1,7 @@
+(ns kaize-tv.core-test
+  (:require [clojure.test :refer :all]
+            [kaize-tv.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
