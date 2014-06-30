af2de0a7e97d599d798b1de0638820893a4c24d1
Anton Kalyaev <anton.kalyaev@gmail.com>
2014-06-30 20:02:28 +0400

    simple hello msg

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject kaize-tv "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject kaize-tv "0.0.1"
+  :description "KaizeTv provides convenient access to Kaize presentations."
+  :url "http://tv.kaize.ru"
+  :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring-jetty-adapter "1.3.0"]])
--- src/kaize_tv/core.clj
+++ src/kaize_tv/core.clj
@@ -1,6 +1,10 @@
-(ns kaize-tv.core)
+(ns kaize-tv.core
+  (:require [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn app [req]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello"})
+
+(defn -main [port]
+  (jetty/run-jetty app {:port (Integer. port) :join? false}))
