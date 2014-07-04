cb4094fea99b50230196eccbab46aa8975c1e7f8
Anton Kalyaev <anton.kalyaev@gmail.com>
2014-07-04 18:40:03 +0400

    html5 boilerplate code
    Downloaded from http://html5boilerplate.com/

--- src/kaize_tv/views/layout.clj
+++ src/kaize_tv/views/layout.clj
@@ -1,21 +1,22 @@
 (ns kaize-tv.views.layout
-  (:use [hiccup.page :only (html5 include-css)]))
+  (:use [hiccup.page :only (html5 include-css include-js)]))
 
 (defn common [title & body]
-  (html5
+  (html5 {:class "no-js"}
    [:head
     [:meta {:charset "utf-8"}]
     [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
-    [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1"}]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
     [:title title]
-    (include-css "/stylesheets/base.css"
-                 "/stylesheets/skeleton.css"
-                 "/stylesheets/screen.css")
+    (include-css "/css/normalize.min.css"
+                 "/css/main.css")
     (include-css "http://fonts.googleapis.com/css?family=Sigmar+One&v1")]
+    (include-js "/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js")
    [:body
-    [:div {:id "header"}
-     [:h1 {:class "container"} "KaizeTv"]]
-    [:div {:id "content" :class "container"} body]]))
+    [:div {:class "header-container"}
+     [:header {:class "wrapper clearfix"}
+      [:h1 {:class "title"} "KaizeTv"]]]
+    [:div {:class "main-container"} [:div {:class "main wrapper clearfix"} body]]]))
 
 (defn not-found []
   (common "Page Not Found"
